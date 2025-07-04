b9c239372212f0d08dc02692ba7e689ebe677a69
Nick Kibre <nkibre@microsoft.com>
2025-07-04 06:20:50 -0700

    add some comments

--- process.catalogitjson.py
+++ process.catalogitjson.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import json, glob, re
 
+# to run this script:
+# export all pages of google drive spreadsheets as TSV, name them "NESFM Silen Film Archive....tsv"
+# export all films from CatalogIt, include all fields, name file "catalogit.allfilms.allfields.json"
+
 def idnorm(idstr):
   idstr = idstr.lower()
   idstr = re.sub(r'[^\w\s]+','.',idstr)
