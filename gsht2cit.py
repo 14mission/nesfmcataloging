79183d789f2efdcb09e05b32dcded90332cb3f18
Nick Kibre <nkibre@microsoft.com>
2025-09-16 11:40:02 -0700

    script to read spreadsheet exports and write catalogit import file

---
+++ gsht2cit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys,os,re
+
+intsvlist = []
+outtsvfn = None
+av = sys.argv[1:]
+ac = 0
+while ac < len(av):
+  if av[ac] == "-h": print("usage: "+sys.argv[0]+" -h(elp) inputfiles*tsv -o outputfile.tsv")
+  elif av[ac] == "-o" and ac+1 < len(av) and av[ac+1][0] != "-": ac+1; outtsvfn = av[ac]
+  elif av[ac][0] == "-": raise Exeption("unkflag: "+av[ac])
+  else: intsvlist.append(av[ac])
+  ac += 1
+
+if outtsvfn == None:
+  print("write to stdout")
+  outh = sys.stdout
+else:
+  print(f"write to {outtsvfn}")
+  outh = open(outtsvfn,"w")
+
+for intsv in intsvlist:
+  print(f"read {intsv}")
+  inh = open(intsv)
+  lnum = 0
+  for ln in inh:
+
+    lnum += 1
