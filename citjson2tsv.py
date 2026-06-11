c5a3dc93361ccf8fdfff5610e48b06e4b92dd953
14mission <nkibre@gmail.com>
2026-06-11 09:14:15 -0700

    support lists

--- citjson2tsv.py
+++ citjson2tsv.py
@@ -7,15 +7,20 @@ fields = sys.argv[2:]
 
 def getfield(obj,field):
     chunks = field.split('.')
+    if len(chunks) > 1:
+        raise Exception(f"hierarchical fields like {field} not supported yet")
     curchunk = chunks[0]
-    if curchunk in obj:
-        return str(obj[curchunk])
-    else:
+    if curchunk not in obj:
         return "NONE"
+    if isinstance(obj[curchunk], list):
+        return "|".join(str(item) for item in obj[curchunk])
+    else:
+        return str(obj[curchunk])
 
 data = None
 with open(jsfn, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
+print("#"+"\t".join(fields))
 for row in data:
     print("\t".join([getfield(row,f) for f in fields]))
